f765209afd5d56d5de66728b1b015d5fd3b77f10
Vladimir <68227685+NoPressF@users.noreply.github.com>
2025-02-14 22:32:40 +0100

    Improvements to the snake movement

--- src/main.rs
+++ src/main.rs
@@ -3,65 +3,44 @@ mod map;
 mod player;
 mod utils;
 
-use std::error::Error;
 use crossterm::cursor::Hide;
 use crossterm::execute;
+use device_query::{DeviceQuery, DeviceState, Keycode};
 use game::Game;
 use map::Map;
 use player::Player;
 use std::io::stdout;
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Instant;
-use winit::event::{DeviceEvent, DeviceId, Ime, MouseButton, MouseScrollDelta, WindowEvent};
-use winit::event_loop::{ActiveEventLoop, EventLoop, EventLoopBuilder};
-use winit::keyboard::{Key, ModifiersState};
 
-
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     execute!(stdout(), Hide).unwrap();
 
     let player = Arc::new(Mutex::new(Player::new()));
     let player_clone = Arc::clone(&player);
     let mut map = Map::new(player.clone());
-
-    let event_loop = EventLoop::new();
-    let (sender, receiver) = mpsc::channel();
-
-    #[cfg(not(web_platform))]
-    {
-        let event_loop_proxy = event_loop.unwrap().create_proxy();
-        let sender = sender.clone();
-        thread::spawn(move || {
-            let event_loop = EventLoopBuilder::<MyEventEnum>::with_user_event().build();
-            let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-            some_channel.send(window);
-
-            event_loop.run(move |event, _, control_flow| {
-                ...
-            })
-        });
-    }
-
+    let device_state = DeviceState::new();
 
     thread::spawn(move || loop {
+        if let Ok(mut player) = player_clone.lock() {
+            let keys = device_state.get_keys();
 
+            for key in keys {
+                match key {
+                    Keycode::W | Keycode::Up => player.change_direction(Some((0, -1))),
+                    Keycode::S | Keycode::Down => player.change_direction(Some((0, 1))),
+                    Keycode::A | Keycode::Left => player.change_direction(Some((-1, 0))),
+                    Keycode::D | Keycode::Right => player.change_direction(Some((1, 0))),
+                    Keycode::Escape => {
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+        }
     });
 
-        // if event::poll(Duration::from_millis(10)).unwrap() {
-        //     if let Ok(character) = stdout.read_key() {
-        //         if let Ok(mut player) = player_clone.lock() {
-        //             match character {
-        //                 console::Key::ArrowUp | console::Key::Char('w') => player.change_direction(Some((0, -1))),
-        //                 console::Key::ArrowDown | console::Key::Char('s') => player.change_direction(Some((0, 1))),
-        //                 console::Key::ArrowLeft | console::Key::Char('a') => player.change_direction(Some((-1, 0))),
-        //                 console::Key::ArrowRight | console::Key::Char('d') => player.change_direction(Some((1, 0))),
-        //                 _ => {}
-        //             }
-        //         }
-        //     }
-        // }
     let mut last_update_snake = Instant::now();
 
     loop {
